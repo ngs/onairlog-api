e2a7ed19da0258c9a2064c6c874209637100bc16
Atsushi Nagase <a@ngs.io>
2020-06-19 18:50:35 +0900

    Set charset

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 		return
 	}
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Write([]byte("OK"))
 	})
 	r.HandleFunc("/siri.txt", func(w http.ResponseWriter, r *http.Request) {
